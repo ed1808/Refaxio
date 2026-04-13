04961d7c94a44541ab28c9acfd59ea4ccde5422d
Edward Rodriguez <ed.rodriguez9408@gmail.com>
2026-04-13 16:26:04 -0500

    Agregar configuración de conexión a la base de datos y repositorios en el contenedor de servicios

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,4 +1,31 @@
+using Backend.Interfaces;
+using Backend.Models;
+using Backend.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Repositories
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IDocumentIdTypeRepository, DocumentIdTypeRepository>();
+builder.Services.AddScoped<IPersonTypeRepository, PersonTypeRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IStorageRepository, StorageRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IProviderRepository, ProviderRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPurchaseRepository, PurchaseRepository>();
+builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+builder.Services.AddScoped<IInventoryRepository, InventoryRepository>();
+builder.Services.AddScoped<IInventoryMovementRepository, InventoryMovementRepository>();
+builder.Services.AddScoped<IPurchaseDetailRepository, PurchaseDetailRepository>();
+builder.Services.AddScoped<ISalesDetailRepository, SalesDetailRepository>();
+builder.Services.AddScoped<ITransferRepository, TransferRepository>();
+builder.Services.AddScoped<ITransferDetailRepository, TransferDetailRepository>();
 
 // Add services to the container.
 
